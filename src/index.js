17080b21ea693dfe70713b03c0e094ea64f838cb
Andrejus Solovjovas <yandrejus.solovjovas@amaris.com>
2018-11-22 10:52:20 +0100

    feat(middleware): adding middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import {createStore} from 'redux';
+import { Provider} from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './store/reducer';
 import './index.css';
 import App from './App';
@@ -17,8 +17,20 @@ this is a good place to wrap everything (culd be as well App.js) */
  * USING THE HIGHER ORDER FUNCTION, PROMISE
  */
 
- const store = createStore(reducer);
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[Middleware] Dispatching', action);
+            const result = next(action);
+            console.log('[Middleware] next state', store.getState())
+            return result;
+        }
+    }
+};
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(logger)));
 
 const app = (
     /* connect the store with redux with our app */
